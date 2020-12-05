856c80c387f2ab441640e5e94c3322974d0248a9
Rio Weber <riodweber@gmail.com>
2020-12-04 23:31:02 -0500

    Ready;

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div @click="darkmode()">
+      <h1 style="font-size: 300px; text-align: center; cursor: pointer;">
+        YES
+      </h1>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
-  }
+    // HelloWorld,
+  },
+  data() {
+    return {
+      dark_mode: false,
+    };
+  },
+  methods: {
+    darkmode() {
+      self.dark_mode = !self.dark_mode;
+      self.dark_mode
+        ? (document.body.className = "dark_mode")
+        : (document.body.className = document.body.className.replace(
+            "dark_mode",
+            ""
+          ));
+    },
+  },
 };
 </script>
 
 <style>
+.dark_mode {
+  color: white;
+  background-color: black;
+}
+
 #app {
+  text-align: center;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
